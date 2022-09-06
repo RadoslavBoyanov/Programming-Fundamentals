ac6af61659988f6b5a8a525053b8ace51959ce8d
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2022-09-06 17:53:52 +0300

    Add a new projects

---
+++ Methods/MathPower/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double theBase = double.Parse(Console.ReadLine());
+            double thePower = double.Parse(Console.ReadLine());
+            double result = RaiseToPower(theBase, thePower);
+            Console.WriteLine(result);
+        }
+
+        static double RaiseToPower(double theBase, double thePower)
+        {
+            double result = Math.Pow(theBase, thePower);
+            return result;
+        }
+    }
+}
---
+++ Methods/Mathoperations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Mathoperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            string operators = Console.ReadLine();
+            int secondNumber = int.Parse(Console.ReadLine());
+            
+            int output = Calculate(firstNumber, operators, secondNumber);
+            Console.WriteLine(output);
+        }
+
+        static int Calculate(int firstNumber, string @operators, int secondNumber)
+        {
+            double result = 0;
+            switch (@operators)
+            {
+                case "/":
+                    result = firstNumber / secondNumber;
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+            }
+
+            return (int)result;
+        }
+    }
+}
