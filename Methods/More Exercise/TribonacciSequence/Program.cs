1dbbe3f2f49effe2531777f3d3e1b4d5400d3a33
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2022-09-09 23:06:45 +0300

    new project

---
+++ Methods/More Exercise/TribonacciSequence/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.ExceptionServices;
+
+namespace TribonacciSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            Tribonacci(inputNumber);
+        }
+
+        private static void Tribonacci(int inputNumber)
+        {
+            if (inputNumber < 1)
+            {
+                return;
+            }
+
+            int a = 1;
+            int b = 1;
+            int c = 2;
+
+            Console.Write(a + " ");
+            
+            if (inputNumber > 1)
+                Console.Write(b + " ");
+
+            if (inputNumber > 2)
+                Console.Write(c + " ");
+
+            for (int i = 3; i < inputNumber; i++)
+            {
+                int curr = a + b + c;
+                a = b;
+                b = c;
+                c = curr;
+                Console.Write(curr + " ");
+            }
+        }
+    }
+}
