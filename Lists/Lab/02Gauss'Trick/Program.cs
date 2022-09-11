47c7d1380a2335840e7ffd7098d104ead069d9ab
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2022-09-11 17:42:12 +0300

    Add new projects with lists

---
+++ Lists/Lab/02Gauss'Trick/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02Gauss_Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+           
+            //List<int> gausInts = new List<int>();
+            int originalLenght = numbers.Count;
+            for (int i = 0; i < originalLenght / 2; i++)
+            {
+                numbers[i] += numbers[numbers.Count - 1];
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+
+            
+            //if (originalLenght % 2 != 0)
+            //{
+            //    numbers.Add(numbers.Count / 2);
+            //}
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
---
+++ Lists/Lab/1SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace _1SumAdjacentEqualNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numberDoubles = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            for (int i = 0; i < numberDoubles.Count - 1; i++)
+            {
+                if (numberDoubles[i] == numberDoubles[i + 1])
+                {
+                    numberDoubles[i] += numberDoubles[i + 1];
+                    numberDoubles.RemoveAt(i + 1);
+                    i = -1;
+                }
+            }
+            Console.WriteLine(string.Join(" ", numberDoubles));
+        }
+    }
+}
