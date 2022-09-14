9679a3088404c83f8835cbb9f4893756806969f2
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2022-09-14 18:05:20 +0300

    add a project

---
+++ Lists/Lab/05RemoveNegativesandReverse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05RemoveNegativesandReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbersList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> newList = new List<int>();
+            int lenght = numbersList.Count;
+            for (int i = 0; i < lenght; i++)
+            {
+                if (numbersList[i] >= 0)
+                {
+                    newList.Add(numbersList[i]);
+                }
+            }
+
+            newList.Reverse();
+            if (newList.Count >= 1)
+            {
+                Console.WriteLine(string.Join(' ', newList));
+            }
+            else Console.WriteLine("empty");
+           
+        }
+    }
+}
