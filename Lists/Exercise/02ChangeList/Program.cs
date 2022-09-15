e041a02c25cadde6b36b1badcfbfb4753135cdd7
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2022-09-15 19:27:28 +0300

    add a project

---
+++ Lists/Exercise/02ChangeList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace _02ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfIntegers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] tokens = command.Split();
+
+                if (tokens.Length == 2)
+                {
+                    int removeNumber = int.Parse(tokens[1]);
+
+                    listOfIntegers.RemoveAll(integer => integer == removeNumber);
+
+                }
+                else if (tokens.Length == 3)
+                {
+                    int number = int.Parse(tokens[1]);
+                    int index = int.Parse(tokens[2]);
+                    listOfIntegers.Insert(index, number);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(' ', listOfIntegers));
+      
+    }
+}
+}
