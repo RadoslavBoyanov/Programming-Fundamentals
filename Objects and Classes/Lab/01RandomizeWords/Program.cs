0480125694cc77d45c54f80aef8bf3a2dc1dfb97
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2022-09-19 19:23:04 +0300

    Projects with objects and classes

---
+++ Objects and Classes/Lab/01RandomizeWords/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _01RandomizeWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] someText = Console.ReadLine().Split(' ');
+
+            Random random = new Random();
+
+            for (int i = 0; i < someText.Length; i++)
+            {
+                int newPosition = random.Next(0, someText.Length);
+
+                string currentPosition = someText[i];
+                someText[i] = someText[newPosition];
+                someText[newPosition]= currentPosition;
+            }
+
+            foreach (var word in someText)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
