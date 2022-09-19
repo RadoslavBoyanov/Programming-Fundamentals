04089e89677a4fd5a2bc429a6ec2972123995840
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2022-09-19 19:12:49 +0300

    add a project

---
+++ Lists/Exercise/07AppendArrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace _07AppendArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrays = Console.ReadLine().Split( '|').Reverse().ToList();
+
+            var numbers = new List<int>();
+
+            foreach (var numberArray in arrays)
+            {
+                numbers.AddRange(numberArray.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
