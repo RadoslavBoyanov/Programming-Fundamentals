87cb493070accac12fc881304c38aceeed6a75ac
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2022-09-24 19:16:14 +0300

    project with object and class

---
+++ Objects and Classes/Exercise/02Articles/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace _02Articles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] articles = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            Article article = new Article(articles[0], articles[1], articles[2]);
+
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] command = Console.ReadLine().Split(": ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (command[0] == "Edit")
+                {
+                    article.Edit(command[1]);
+                }
+                else if (command[0] == "ChangeAuthor")
+                {
+                    article.ChangeAuthor(command[1]);
+                }
+                else if (command[0] == "Rename")
+                {
+                    article.Rename(command[1]);
+                }
+            }
+
+            Console.WriteLine(article.ToString());
+        }
+
+        class Article
+        {
+            public Article(string title, string content, string author)
+            {
+                this.Title = title;
+                this.Content = content;
+                this.Author = author;
+            }
+
+            public string Title { get; set; }
+            public string Content { get; set; }
+            public string Author { get; set; }
+
+            public void Edit(string newContent)
+            {
+                this.Content = newContent;
+            }
+
+            public void ChangeAuthor(string newAuthor)
+            {
+                this.Author = newAuthor;
+            }
+
+            public void Rename(string newTitle)
+            {
+                this.Title = newTitle;
+            }
+
+            public override string ToString()
+            {
+                return $"{this.Title} - {this.Content}: {this.Author}";
+            }
+        }
+    }
+}
