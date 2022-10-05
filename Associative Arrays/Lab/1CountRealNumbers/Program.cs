c661db0306ef98eaf132ffd959761220df68aaa1
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2022-10-05 23:19:35 +0300

    Folder with Associative Arrays

---
+++ Associative Arrays/Lab/1CountRealNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split(" ").Select(double.Parse).ToArray();
+
+            SortedDictionary<double, int> sorted = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (sorted.ContainsKey(number))
+                {
+                    sorted[number]++;
+                }
+                else
+                {
+                    sorted.Add(number, 1);
+                }
+            }
+
+            foreach (var sort in sorted)
+            {
+                Console.WriteLine($"{sort.Key} -> {sort.Value}");   
+            }
+        }
+    }
+}
