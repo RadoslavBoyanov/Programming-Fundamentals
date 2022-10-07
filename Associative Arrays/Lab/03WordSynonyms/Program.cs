c1f0e2c1a9025978bc0680c14846dddea379f234
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2022-10-07 20:49:10 +0300

    add a project

---
+++ Associative Arrays/Lab/03WordSynonyms/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03WordSynonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            var wordSynonyms = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var word = Console.ReadLine();
+                var synonim = Console.ReadLine();
+
+                if (wordSynonyms.ContainsKey(word) == false)
+                {
+                    wordSynonyms.Add(word, new List<string>());
+                }
+                wordSynonyms[word].Add(synonim);
+            }
+
+            foreach (var word in wordSynonyms)
+            {
+                Console.WriteLine($"{word.Key} - {String.Join(", ", word.Value)}");
+            }
+        }
+    }
+}
