1dd9865d349f11281d8cd6359a99d41150c0a41c
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2022-10-12 19:54:32 +0300

    Add a project

---
+++ Associative Arrays/Exercise/1CountCharsinaString/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1CountCharsinaString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> wordsList = Console.ReadLine().Split(" ").ToList();
+
+            Dictionary<string, int> occurrences = new Dictionary<string, int>();
+
+            for (int i = 0; i < wordsList.Count; i++)
+            {
+                string currentWord = wordsList[i];
+                for (int j = 0; j < currentWord.Length; j++)
+                {
+                    char charecter = currentWord[j];
+                    if (occurrences.ContainsKey(charecter.ToString()))
+                    {
+                        occurrences[charecter.ToString()]++;
+                    }
+                    else
+                    {
+                        occurrences.Add(charecter.ToString(), 1);
+                    }
+                }
+            }
+
+            foreach (var wordOccurrence in occurrences)
+            {
+                Console.WriteLine($"{wordOccurrence.Key} -> {wordOccurrence.Value}");
+            }
+        }
+    }
+}
