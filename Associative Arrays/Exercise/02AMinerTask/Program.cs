00f56995a911953fc4499cc0e06b6f0347f1ed34
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2022-10-12 19:56:21 +0300

    add a project

---
+++ Associative Arrays/Exercise/02AMinerTask/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+
+namespace _02AMinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordLine = Console.ReadLine();
+
+            Dictionary<string, int> resourcesQuantity = new Dictionary<string, int>();
+
+            
+
+            while (wordLine != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (resourcesQuantity.ContainsKey(wordLine))
+                {
+                    resourcesQuantity[wordLine] += quantity;
+                }
+                else
+                {
+                    resourcesQuantity[wordLine] = quantity;
+                }
+
+                wordLine = Console.ReadLine();
+            }
+
+            foreach (var item in resourcesQuantity)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
