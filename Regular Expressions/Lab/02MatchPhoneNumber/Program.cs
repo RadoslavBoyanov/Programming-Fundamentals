d0dc033b935bb4c91ec6b9c8666bfa4092aa742a
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2022-10-17 22:27:23 +0300

    Regular expressions - lab projects

---
+++ Regular Expressions/Lab/02MatchPhoneNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"[+]359 2 [0-9]{3} [0-9]{4}\b|[+]359-2-[0-9]{3}-[0-9]{4}\b";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
---
+++ Regular Expressions/Lab/03MatchDates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string date = Console.ReadLine();
+
+            string pattern = @"\b(?<day>[0-9]{2})([./\-])(?<month>[A-Z][a-z]{2})\1(?<year>[0-9]{4})";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(date);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"Day: {match.Groups["day"].Value}, Month: {match.Groups["month"].Value}, Year: {match.Groups["year"].Value}");   
+            }
+        }
+    }
+}
